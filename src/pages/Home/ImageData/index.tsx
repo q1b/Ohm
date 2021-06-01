d82530d74ab906170ba55d713a96671173f093e1
sacarvy <sukhpreetben10@gmail.com>
2021-06-01 22:44:19 +0530

    Updated Of last Commi

---
+++ src/pages/Home/ImageData/index.tsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+import Image1 from '../Images/1.jpg'
+import Image2 from '../Images/2.jpg'
+
+export default [
+	{
+		index:1,
+		url:Image1,
+		header:'Buddha Purnima',
+		content:'Buddha embodied silence & compassion, and the way for this is through Meditation. In this trying time, these values are more relevant.',
+	},
+	{
+		index:2,
+		url:Image2,
+		header:'Buddha Purnima',
+		content:'Buddha embodied silence & compassion, and the way for this is through Meditation. In this trying time, these values are more relevant.',
+	}
+
+]
