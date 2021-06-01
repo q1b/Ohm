857cf722e35a5d297af7ca34368362490a2e8c5d
sacarvy <sukhpreetben10@gmail.com>
2021-06-01 22:52:15 +0530

    Updated Of last Commi

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,16 @@
-import React,{useState} from "react"
-import Image1 from '../../lib/Images/SriSri_White_Cloths_Brown_Sofa.jpg'
-import Image2 from '../../lib/Images/SriSriRaviShankar_Profile.jpg'
+import React from "react"
+import Image from '../../lib/Images/SriSri_White_Cloths_Brown_Sofa.jpg'
+
 export default function Home() {
-	let [Image,setImage] = useState(Image1)
-	function NextImage(){
-		setTimeout(function(){ setImage(Image = Image2) }, 3000);
-	}
 	return(
-	<>
    <section className="h-[calc(100vh-96px)] w-full flex items-end justify-center">
 				<article className="relative -top-10 bg-white dark:bg-green-500 p-4 dark:bg-opacity-20 z-10 bg-opacity-20 w-[300px] lg:w-[675px] backdrop-filter backdrop-blur rounded-lg">			
 							<h1 className="text-blueGray-900 dark:text-green-700 text-xl lg:text-4xl font-semibold" style={{textShadow:'0.5px 0.5px 1px #00FFAA'}}>Buddha Purnima</h1>
 							<p  className="text-coolGray-900 dark:text-green-100 mt-2 text-base lg:text-xl" style={{textShadow:'0.5px 0.5px 2px #000'}}>Buddha embodied silence & compassion, and the way for this is through Meditation. In this trying time, these values are more relevant.</p>
 				</article>
 				<article className="absolute flex items-center justify-center h-[calc(100%-96px)] w-[calc(100%-10px)] top-[96px]">
-					<img className="h-[97%] md:w-auto rounded-lg mt-2 md:mt-1 object-contain" src={Image} alt="" />
+					<img className="h-[97%] md:w-[calc(90%)] rounded-lg mt-2 md:mt-1 object-cover" src={Image} alt="" />
 				</article>
 	</section>
-	<button className="cursor-pointer relative z-20 w-20 h-20 bg-white" onClick={NextImage}/>
-	</>
 	)
 }
