073a2e2f3309d954a31d2376cb18426ce2214d2c
sacarvy <sukhpreetben10@gmail.com>
2021-06-02 23:28:34 +0530

    Added Sri Sri First Story

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
     {/*<Banner />*/}
-      <section className="flex flex-col w-full h-[900px] bg-coolGray-800 dark:bg-lime-100 ">
+      <section className="flex flex-col place-content-around w-full bg-coolGray-800 dark:bg-lime-100 ">
         <header className="sticky z-50 px-2 flex top-0 bg-blueGray-900 dark:bg-lime-400 !bg-opacity-50 h-[96px] place-content-around items-center w-full backdrop-filter backdrop-blur">
           <Header />
         </header>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";;
+import {SunIcon,MoonIcon,GlobeIcon} from '@heroicons/react/outline';
 import Data from "./ImageData";
 import ImageS from "../../lib/Images/SriSriRaviShankar_Profile.jpg";
 
 import { Transition } from "@headlessui/react";
-function Home(): any {
+function Home() {
   const [index, setIndex] = useState(0);
   const [header, setHeader] = useState("Me is You");
   const [content, setContent] = useState(
@@ -22,25 +23,25 @@ function Home(): any {
   useEffect(() => {
     setTimeout(function () {
       setVisi(false);
-    }, 2250);
+    }, 1000);
     setTimeout(function (): void {
       const Datas: any = Data[index];
       setVisi(true);
       setImage(Datas.url);
       setContent(Datas.content);
       setHeader(Datas.header);
-    }, 3000);
+    }, 2000);
   }, [index]);
 
   useEffect(() => {
     setTimeout(function () {
       ChangeIndex();
-    }, 5000);
+    }, 7000);
   });
 
   return (
     <>
-      <section className="h-[220vh] w-full flex items-center justify-center">
+      <section className="h-[100vh] w-full flex items-center justify-center">
         <article className="absolute flex items-center justify-center h-[calc(100%-96px)] w-[calc(100%-10px)] top-[96px]">
           <div className="h-[97%] flex items-center justify-center md:w-[calc(90%)]  mt-2 md:mt-1 ">
             <Transition
@@ -49,7 +50,7 @@ function Home(): any {
               enter="transition-scale transform duration-500 ease-in-out"
               enterFrom="opacity-0 scale-50"
               enterTo="opacity-100 scale-100"
-              leave="transition-scale transform duration-500 ease-in-out"
+              leave="transition-scale transform duration-1000 ease-in-out"
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-50"
             >
@@ -89,10 +90,47 @@ function Home(): any {
         </article>
       </section>
       
-      <section className="w-full h-screen bg-white">
-        <article></article>
+      <section className="w-full grid grid-cols-1 place-items-center min-h-[120vh] py-10 bg-[#000C15] dark:bg-white mt-10">
+        <div className="mx-10">
+          <h1 className="text-white underline font-round text-4xl">Some Light Over What Sri Sri Did in Past</h1>
+        </div>
+        <section className="bg-gradient-to-br from-indigo-600 via-purple-500 to-pink-500 dark:bg-orange-400 mt-10 w-[80vw] rounded-lg">
+      {/*Grid to Show all Main DisCourse*/}
+      <section className="flex place-content-between p-4">
+        <h1 className="text-7xl font-sans font-bold underline text-indigo-900"> Making Impact </h1>
+        <article className="flex flex-shrink-0 w-max">
+          <div className="">
+            <SunIcon   className={`lg:w-16 text-white dark:text-white ` }  />
+          </div>
+          <div className="absolute hidden">
+            <GlobeIcon className={`lg:w-16 text-white dark:text-white `}/>
+          </div>
+          <div className="absolute hidden">
+            <MoonIcon  className={`lg:w-16 text-white dark:text-white ` }  />
+          </div>
+        </article>
+      </section>
+        <div className="p-4">
+        <blockquote className="p-2 mx-1 bg-indigo-600 text-white rounded-lg">
+          The Colombian Government and the Revolutionary Armed Forces of Colombia (FARC) signed a historic peace pact that ends a 52-year-old armed conflict yesterday. Playing an important role in the peace process was the Art of Living founder and humanitarian – Sri Sri Ravi Shankar.
+        </blockquote>
+<h1 className="p-2 text-white text-[1.09rem] mt-2 font-round rounded-lg">Last year, the spiritual leader had travelled to Cuba, Havana and Colombia, talking to both the parties, building trust between them. Last year, Sri Sri travelled to Cuba to meet the representatives of FARC, including the leader of the FARC-EP delegation, Iván Márquez. His efforts resulted in a major breakthrough in June 2015 when the FARC rebels announced a unilateral ceasefire.
+The strife had taken the lives of 220,000 people and displaced more than 5 million.
+
+The government and the rebels had been talking peace since 2012, and had reached agreements on land reforms and drug trade. But what was needed was the healing touch of a spiritual leader that addressed the core values of the peace process. President Juan Manuel Santos remembered, "I had the great joy of speaking with him about his efforts to hopefully end an internal armed conflict that has lasted more than 50 years. Later on, with my consent, he met in Havana and Cuba with the negotiators of the FARC leaders to encourage them to follow the Gandhian principle of non-violence, and to cultivate the art of meditation and breathing."
+
+Márquez acknowledged that Sri Sri's teachings about the Gandhian principle of non-violence inspired them.</h1>
+<button className="m-2 p-2 bg-white rounded-lg text-purple-600">Read more</button>
+        </div>      
+        </section>
       </section>
     </>
   );
 }
 export default Home;
+
+// "With the help of Sri Sri, we have placed our spirit to achieve reconciliation and coexistence of a big and benevolent country, whose destiny can’t be that of the war. The teachings of the Art of Living are essential to achieve a stable and long-lasting peace."
+
+// One year later, yesterday, the peace pact has been signed. Gurudev Sri Sri Ravi Shankar on Wednesday congratulated Juan Manuel Santos, the Colombian President and Marcos Carratala, the FARC negotiator for announcing a bilateral ceasefire.
+
+// During last one year, Sri Sri had meetings and dialogues with both parties (See the timeline below). He taught his stress-elimination techniques to members of the Colombian government as well as the FARC that gave them the experience of peace and struck a chord within them.
